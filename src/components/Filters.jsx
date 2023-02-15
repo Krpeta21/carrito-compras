7e9eff0e8f63ff0067a08abf4bdf2322dd9a9c75
IrvingGlt <41974385+Krpeta21@users.noreply.github.com>
2023-02-15 16:19:21 -0600

    carrito de compras

---
+++ src/components/Filters.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './Filters.css'
+export function Filters({onChange}){
+    const [minPrice, setMinPrice] = useState(0)
+
+    const handleChangeMinPrice = (event) =>{       
+        setMinPrice(event.target.value)
+        onChange(preventState=>({
+            ...preventState,
+            minPrice: event.target.value
+        }))
+    }
+    const handleChangeCategory = (event) =>{
+        onChange(preventState=>({
+            ...preventState,
+            category: event.target.value
+        }))
+    }
+    return(
+        <section className="filters">
+            <div>
+                <label htmlFor="price">Price</label>
+                <input type="range" id='price' min='0' max= '1000' onChange={handleChangeMinPrice} />
+                <span>${minPrice}</span>
+            </div>
+            <div>
+                <label htmlFor="category">Categoria </label>
+                <select onChange={handleChangeCategory} id="category">
+                    <option value="all">Todas</option>
+                    <option value="laptops">Portatiles</option>
+                    <option value="smartphones">Celulares</option>
+                </select>
+            </div>
+        </section>
+    )
+}
