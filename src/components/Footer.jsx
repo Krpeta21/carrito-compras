a18537c22aa94ccbaf9b9a03d79862cdb3abeed5
IrvingGlt <41974385+Krpeta21@users.noreply.github.com>
2023-02-15 22:48:56 -0600

    carrito funcionalidad agregar y quitar producto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
+import { useCart } from '../hooks/useCart'
 import { useFilters } from '../hooks/useFilters'
 import './Footer.css'
 export function Footer(){
     const {filters} = useFilters()
+    const {cart} = useCart()
     return(
         <footer className='footer'>
-            {
+            {/* {
                 JSON.stringify(filters,null,2)
+            } */}
+            {
+                JSON.stringify(cart,null,2)
             }
-            {               
+            {        
+
             // <h4>Prueba Tecnica de react</h4>
             // <span>@Krpeta21</span>
             // <h5>Shopping cart con useContext y useReducer</h5>
